7b0dfe58f3a37ec5226e734a0a3dd1c5ec013a6d
Felipe Antônio <felipeantonio.superiron@gmail.com>
2021-09-06 13:59:30 -0300

    git commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*Scroll Reveal Animation*/
+ScrollReveal().reveal('.kayky', { interval: 600 });
+ScrollReveal().reveal('.form', {delay:100});
+
+
+ScrollReveal().reveal('.container-single', {
+    rotate:{
+        x:20,
+        z:20
+    }
+})
