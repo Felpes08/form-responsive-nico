a650aebeff154f5cbee99e16b9108e72e406c664
Felipe Antonio de Araujo Santos <felipe.araujo@santosbrasil.com.br>
2021-09-07 01:22:29 -0300

    ultimo commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*Scroll Reveal Animation*/
+ScrollReveal().reveal('.kayky', { interval: 600 });
+ScrollReveal().reveal('.form', {delay:100});
+
+ScrollReveal().reveal(',icon', {duration:2000})
+ScrollReveal().reveal('.icon', {
+    rotate:{
+        x:40,
+        z:40
+    }
+})
